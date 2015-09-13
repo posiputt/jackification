d8198c4b53a78e556a46d72d75b14af65b2f461a
posiputt <i@posiputt.de>
2015-09-13 23:34:22 +0200

    removed unnecessary import time

--- jackification.py
+++ jackification.py
@@ -2,26 +2,27 @@
 
 import gtk
 import gobject
-import time
 from os.path import expanduser
 
 path = expanduser("~") + "/.config/ocaml-xmpp-client/notification.state"
+offline = gtk.STOCK_NO
+online = gtk.STOCK_YES
 icon = gtk.StatusIcon()
-icon.set_from_stock(gtk.STOCK_NO)
 icon.set_visible(True)
+icon.set_from_stock(gtk.STOCK_NO)
 
 def show_icon():
+    gobject.timeout_add(1000, show_icon)
     with open(path, "r") as f:
         status = f.read()
         if status[:9] == "connected":
-            icon.set_from_stock(gtk.STOCK_YES)
+            icon.set_from_stock(online)
             if status[10:] == "notifications":
                 icon.set_blinking(True)
             else:
                 icon.set_blinking(False)
         else:
-            icon.set_from_stock(gtk.STOCK_NO)
-        gobject.timeout_add(1000, show_icon)
+            icon.set_from_stock(offline)
 
 if __name__ == '__main__':
     show_icon()
