98930ffaa47d9c5c218bf8f9851f31879a1ee333
posiputt <i@posiputt.de>
2015-09-13 16:27:53 +0200

    completed readme, completed jackification.py

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # jackification
 a very simple systray icon for jackline (https://github.com/hannesm/jackline)
+
+needs pyGTK >= 2.10 to be installed.
+
+usage: run script. if jackline is running, a tray icon will be visible. if there are notifications, the tray icon will blink.
---
+++ jackification.py
@@ -0,0 +1,27 @@
+import gtk
+import gobject
+import time
+from os.path import expanduser
+
+path = expanduser("~") + "/.config/ocaml-xmpp-client/notification.state"
+icon = gtk.StatusIcon()
+icon.set_from_stock(gtk.STOCK_NO)
+icon.set_visible(True)
+
+def show_icon():
+    with open(path, "r") as f:
+        status = f.read()
+        if status[:9] == "connected":
+            icon.set_from_stock(gtk.STOCK_YES)
+            if status[10:] == "notifications":
+                icon.set_blinking(True)
+            else:
+                icon.set_blinking(False)
+        else:
+            icon.set_from_stock(gtk.STOCK_NO)
+        gobject.timeout_add(1000, show_icon)
+
+if __name__ == '__main__':
+    while True:
+        gobject.timeout_add(1000, show_icon)
+        gtk.main()
